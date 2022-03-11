4996b84e49b329ac60d66e676687936a35a589b8
Darudul <darudulislam@gmail.com>
2022-03-12 00:51:17 +0800

    added all

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ const addToLiked = (id) => {
   likedPostsId.push(id);
   likedPostsId.includes(id);
   showPosts(posts);
-  reportPost();  
+  reportPost();
 };
 
 const reportPost = (id) => {
@@ -42,16 +42,19 @@ const switchTab = (id) => {
     document.getElementById("posts").style.display = "grid";
     document.getElementById("liked").style.display = "none";
     document.getElementById("reported").style.display = "none";
+    document.getElementById("fixed-bonus").style.display = "block";
   } else if (id === "liked") {
     document.getElementById("liked").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("reported").style.display = "none";
+    document.getElementById("fixed-bonus").style.display = "none";
 
     displayLikedPosts();
   } else {
     document.getElementById("reported").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("liked").style.display = "none";
+    document.getElementById("fixed-bonus").style.display = "none";
 
     displayReportedPosts();
   }
